e8f4ec39805cd33f193291aa74264e807b2ea25b
Carlos Erickson Andre Olivares Conza <carlos.olivares@tecsup.edu.pe>
2024-05-06 12:37:21 -0700

    Initial Commit

---
+++ OlivaresColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OlivaresColeccionDeJuegos
+//
+//  Created by Carlos Erickson Olivares Conza on 6/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
